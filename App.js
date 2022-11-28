9897e4f645f514bf662a4e1f93a31d4986aa9462
Daniel <shadeldaniel@gmail.com>
2022-11-27 18:49:12 -0700

    Added home screen, buttons, navigation, and styles

--- App.js
+++ App.js
@@ -6,27 +6,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './components/Home';
 import Quiz from './components/Quiz';
+import Learn from './components/Learn';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+export default App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Learn" component={Learn} />
         <Stack.Screen name="Quiz" component={Quiz} />
+        {/* TODO: Change components */}
+        <Stack.Screen name="Metronome" component={Home} />
+        <Stack.Screen name="Tuner" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-
