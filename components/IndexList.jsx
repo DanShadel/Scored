891c4a205abf9e14e9f71b5fca9e9c9c45754024
Dan Shadel <shadeldaniel@gmail.com>
2022-12-25 21:43:31 -0500

    added linter

--- components/IndexList.jsx
+++ components/IndexList.jsx
@@ -1,85 +1,83 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
-import { useSelector } from 'react-redux';
-import { chromaticScaleAllVariations, majorScales, minorScales} from '../constants/musicConstants';
-import { getTriadWithRange } from '../helpers/helpers';
-import { Note } from '../helpers/Note';
-import { getAccidental, getClef, getRange } from '../helpers/selectors';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {Scale} from 'tonal';
+import {majorScales, minorScales} from '../constants/musicConstants';
+import {getTriadWithRange} from '../helpers/helpers';
+import {Note} from '../helpers/Note';
+import {getAccidental, getClef, getRange} from '../helpers/selectors';
 import IndexItem from './IndexItem';
 import StaveControls from './StaveControls';
-import { Scale } from 'tonal';
 
 const generateRows = (selection, clef, range, accidental) => {
-    let notes = [];
-    let nextOctave = false;
+	let notes = [];
+	let nextOctave = false;
 
-    if (selection === 'notes') {
-        notes = Scale.get('C major').notes;
-        return notes.map((note, index) => (<IndexItem key={index} label={note+accidental} notes={[new Note(note+accidental, range)]} clef={clef} beats={1} />))
-    } else if (selection === 'chords') {
-        return Object.keys(scaleList).map((key, index) => {
-            notes = getTriadWithRange(scaleList[key], range).map((note) => (new Note(note.name, note.range)))
-            return (<IndexItem key={index} label={key} clef={clef} notes={notes} beats={1} />)
-        })
-    } else if (selection === 'scales') {
+	if (selection === 'notes') {
+		notes = Scale.get('C major').notes;
+		return notes.map((note, index) => (<IndexItem key={index} label={note+accidental} notes={[new Note(note+accidental, range)]} clef={clef} beats={1} />));
+	} else if (selection === 'chords') {
+		//fix later
+		return Object.keys(scaleList).map((key, index) => {
+			notes = getTriadWithRange(scaleList[key], range).map((note) => (new Note(note.name, note.range)));
+			return (<IndexItem key={index} label={key} clef={clef} notes={notes} beats={1} />);
+		});
+	} else if (selection === 'scales') {
 
-        return minorScales.map((scaleName, index) => {
-            const notesInScale = Scale.get(scaleName).notes; 
-            notes = notesInScale.map((note, noteInd) => {   
+		return minorScales.map((scaleName, index) => {
+			const notesInScale = Scale.get(scaleName).notes;
+			notes = notesInScale.map((note, noteInd) => {
 
-                if (note[0] === 'C' && noteInd > 0) {
-                    nextOctave = true;
-                }
-                
-                const noteRange = nextOctave ? range + 1 : range
-                return (new Note(note, noteRange))
-            });
+				if (note[0] === 'C' && noteInd > 0) {
+					nextOctave = true;
+				}
 
-            nextOctave = false;
-            notes.push(new Note(notesInScale[0], range+1))
+				const noteRange = nextOctave ? range + 1 : range;
+				return (new Note(note, noteRange));
+			});
 
-            return (<IndexItem key={index} label={scaleName} clef={clef} notes={notes} beats={4} />)
-        })  
-    } else if (selection === 'keys') {
+			nextOctave = false;
+			notes.push(new Note(notesInScale[0], range+1));
 
-        return majorScales.map((scaleName, index) => {
-            const notesInScale = Scale.get(scaleName).notes; 
-            return (<IndexItem key={index} label={scaleName} clef={clef} notes={[]} beats={0} keySignature={notesInScale[0]} />)
-        })
-    }
-}
+			return (<IndexItem key={index} label={scaleName} clef={clef} notes={notes} beats={4} />);
+		});
+	} else if (selection === 'keys') {
 
-const IndexList = ({ route }) => {
-    const { selection } = route.params;
-    const range = useSelector(getRange);
-    const clef = useSelector(getClef);
-    const accidental = useSelector(getAccidental);
-    return (
-        <View style={styles.container}>
-            <ScrollView style={styles.scroll}>
-                {generateRows(selection, clef, range, accidental)}
-            </ScrollView>
-            <View style={styles.controls}>
-                <StaveControls showAccidentals={selection === 'notes'} />
-            </View>
-        </View>
-    );
+		return majorScales.map((scaleName, index) => {
+			const notesInScale = Scale.get(scaleName).notes;
+			return (<IndexItem key={index} label={scaleName} clef={clef} notes={[]} beats={0} keySignature={notesInScale[0]} />);
+		});
+	}
+};
+
+const IndexList = ({route}) => {
+	const {selection} = route.params;
+	const range = useSelector(getRange);
+	const clef = useSelector(getClef);
+	const accidental = useSelector(getAccidental);
+	return (
+		<View style={styles.container}>
+			<ScrollView style={styles.scroll}>
+				{generateRows(selection, clef, range, accidental)}
+			</ScrollView>
+			<View style={styles.controls}>
+				<StaveControls showAccidentals={selection === 'notes'} />
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    scroll: {
-        flex: 1,
-        flexDirection: 'column',
-    },
-    controls: {
-        flex: .3,
-
-    }
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	scroll: {
+		flex: 1,
+		flexDirection: 'column',
+	},
+	controls: {flex: .3,}
 });
 
 export default IndexList;
--- constants/musicConstants.js
+++ constants/musicConstants.js
@@ -1,37 +1,37 @@
-import { Scale } from "tonal";
+import {Scale} from 'tonal';
 
-export const clefs = ['treble', 'bass']
+export const clefs = ['treble', 'bass'];
 
 // Note Ranges on each clef (kinda)
-export const trebleRange = [4, 5]
-export const bassRange = [2, 3,]
+export const trebleRange = [4, 5];
+export const bassRange = [2, 3,];
 
 export const majorScales = [
-    'C major',
-    'G major',
-    'D major',
-    'A major',
-    'E major',
-    'B major',
-    'F major',
-    'Bb major',
-    'Eb major',
-    'Ab major',
-    'Db major',
-    'Gb major',
-]
+	'C major',
+	'G major',
+	'D major',
+	'A major',
+	'E major',
+	'B major',
+	'F major',
+	'Bb major',
+	'Eb major',
+	'Ab major',
+	'Db major',
+	'Gb major',
+];
 
 export const minorScales = [
-    'C minor',
-    'G minor',
-    'D minor',
-    'A minor',
-    'E minor',
-    'B minor',
-    'F minor',
-    'Bb minor',
-    'Eb minor',
-    'Ab minor',
-    'Db minor',
-    'Gb minor',
-]
+	'C minor',
+	'G minor',
+	'D minor',
+	'A minor',
+	'E minor',
+	'B minor',
+	'F minor',
+	'Bb minor',
+	'Eb minor',
+	'Ab minor',
+	'Db minor',
+	'Gb minor',
+];
