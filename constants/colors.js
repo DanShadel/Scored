f1d0cbe0cc684bfa70eca2f5e3286f6469951ec2
Dan Shadel <shadeldaniel@gmail.com>
2024-04-18 13:57:57 -0600

    multiple choice, initial build, store refactor

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    light: '#777777',
+    veryLight: '#AAAAAA',
+    heavy: "#444444",
+    veryHeavy: "#111111",
+    paperWhite: '#EEEEEE',
+    greenComplete: '#00aa11',
+    redFail: '#CC0000',
+    white: '#FFFFFF',
+    black: '#000000',
+}
